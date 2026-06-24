d261f75f396e22ab421da66ab68abc1fc263d855
jlhuahin20-sys <jlhuaahin20@gmail.com>
2026-06-24 16:58:03 +0700

    ส่งงาน Workshop2_1

---
+++ ws2_1.ts
@@ -0,0 +1,10 @@
+let age: number = 12;
+console.log("Age:", age);
+
+if (age < 12) {
+    console.log("Price is $5");
+} else if (age <= 60) {
+    console.log("Price is $10");
+} else {
+    console.log("Price is $7");
+}
